45d12275a0a34669d019901ca822f6877518bfff
Quentin Jungo <quentin.jungo@gmail.com>
2023-04-04 11:12:32 +0200

    Update run.c

--- src/run.c
+++ src/run.c
@@ -1,25 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   run.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qjungo <qjungo@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/04 11:12:22 by qjungo            #+#    #+#             */
+/*   Updated: 2023/04/04 11:12:26 by qjungo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <X11/X.h>
 #include "cube3D.h"
-
-/*
-**  hook funct are called as follow :
-**
-**   expose_hook(void *param);
-**   key_hook(int keycode, void *param);
-**   mouse_hook(int button, int x,int y, void *param);
-**   loop_hook(void *param);
-**
-*/
-
 
 int	loop_hook(void *data)
 {
 	t_program *program = (t_program *)data;
 
-	//printf("loop hook\n");
 	if (!program->refresh)
 		return 0;
+	printf("loop hook refresh\n");
+
+	program->refresh = FALSE;
 	return 0;
 }
 
@@ -52,17 +53,7 @@ void	run(t_program *program)
 	mlx_hook(program->win, ON_DESTROY, 0, close_window, program);
 	mlx_hook(program->win, ON_KEYDOWN, 1L<<0, key_hook, program);
 
-	//mlx_loop_hook(program->mlx, loop_hook, program);
+	mlx_loop_hook(program->mlx, loop_hook, program);
 	mlx_loop(program->mlx);
 	printf("close window\n");
 }
-
-/*
-void	main_loop(t_program *program)
-{
-
-	mlx_hook(program->win, ON_DESTROY, 0, close_window, NULL);
-	mlx_hook(program->win, ON_KEYDOWN, 0, key_hook, NULL);
-	mlx_loop(program->mlx);
-}
-*/
